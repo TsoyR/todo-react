070cfa87d9872e0c5103da1f395bdbeb79dfe791
roman <tsoyroman29@gmail.com>
2023-08-26 15:04:31 +0900

    Updates

---
+++ precache-manifest.00de6a2a9e5671b0c758b983653d0c86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8095b7a30ff0881c9534edd689954528",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5a3ac8ac1305b03e722",
+    "url": "/static/css/main.8eb2b0f7.chunk.css"
+  },
+  {
+    "revision": "a8b370b248bb1d550ead",
+    "url": "/static/js/2.0399c624.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0399c624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5a3ac8ac1305b03e722",
+    "url": "/static/js/main.df7ab93e.chunk.js"
+  },
+  {
+    "revision": "a34c2041f225747bc42b",
+    "url": "/static/js/runtime-main.07cae033.js"
+  }
+]);
